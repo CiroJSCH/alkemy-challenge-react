40bfe2ff627ce50c664b392d026283d755547570
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2022-10-08 15:48:13 -0300

    Estilos al header con bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Search from "./Search";
 
 const Header = (props) => {
   return (
     <header>
-        <nav>
-            <ul>
-                <li>Listado</li>
-                <li>
-                    <p>Favoritos</p>
-                    <span>{props.favorites.length}</span>
-                </li>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            ALKEFLIX
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor02"
+            aria-controls="navbarColor02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" to={"/movie-list"}>
+                  Listado
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to={"/favorites"}>
+                  Favoritos: {props.favorites.length}
+                </Link>
+              </li>
             </ul>
-        </nav>
+            <Search />
+          </div>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
